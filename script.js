6b84af5ab28c519b15b5c32938b08c10936cac23
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-02-26 10:17:35 +0800

    fix: polish logic for creating the grid

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let init = 'Hello World';
 console.log(init);
 
-let numberOfSquares = 24;
+let numberOfSquares = 70;
 let container = document.querySelector(".inside");
 
+// for (let i = 0; i < numberOfSquares; i++){
+//     console.log(`squares: ${numberOfSquares}`);
+//     let newSquare = document.createElement('div');
+//     newSquare.classList.add("square");
+//     container.appendChild(newSquare);
+//  }
+
+// logic for creating grid based on input (numberOfSquares)
 for (let i = 0; i < numberOfSquares; i++){
-    console.log(`squares: ${numberOfSquares}`);
-    let newSquare = document.createElement('div');
-    newSquare.classList.add("square");
-    container.appendChild(newSquare);
+    let newRow = document.createElement("div")
+    newRow.classList.add("row");
+    container.appendChild(newRow);
+    for(let j = 0; j < numberOfSquares; j++){
+      console.log(`squares: ${numberOfSquares}`);
+      let newSquare = document.createElement('div');
+      newSquare.classList.add("square");
+      newRow.appendChild(newSquare);
+    }
 }
