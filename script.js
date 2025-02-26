f8d6c6bf37b16dbe40cc0bfa6c6cbe722ec04b6b
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-02-26 10:00:49 +0800

    feat: initial logic for creating divs

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let init = 'Hello World';
+console.log(init);
+
+let numberOfSquares = 24;
+let container = document.querySelector(".inside");
+
+for (let i = 0; i < numberOfSquares; i++){
+    console.log(`squares: ${numberOfSquares}`);
+    let newSquare = document.createElement('div');
+    newSquare.classList.add("square");
+    container.appendChild(newSquare);
+}
